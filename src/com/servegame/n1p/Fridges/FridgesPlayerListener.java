eead9a92265eb2746e2f486b04e2d60cd769850d
xXN1pplefaceXx <jayjay110@hotmail.com>
2011-05-19 23:47:41 +1000

    Fixed errors I forgot to do before :3

--- src/com/servegame/n1p/Fridges/FridgesPlayerListener.java
+++ src/com/servegame/n1p/Fridges/FridgesPlayerListener.java
@@ -5,13 +5,11 @@ import net.minecraft.server.EntityPlayer;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.block.Chest;
 import org.bukkit.block.ContainerBlock;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.craftbukkit.inventory.CraftInventory;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
-import org.bukkit.inventory.ItemStack;
 
 public class FridgesPlayerListener extends PlayerListener {
 
@@ -24,84 +22,69 @@ public class FridgesPlayerListener extends PlayerListener {
 
 		if (plugin.enabled){
 			
-			Block block = event.getClickedBlock();
+			if (event.getClickedBlock() instanceof Block){
 			
-			if (block.getType() == Material.IRON_BLOCK){
+				Block block = (Block) event.getClickedBlock();
 				
-			    EntityPlayer entityPlayer = ((CraftPlayer)event.getPlayer()).getHandle();
-				
-				Location loc = block.getLocation();	
-				loc.setX(loc.getX() + 1);	
-				Block x1 = loc.getWorld().getBlockAt(loc);
-				
-				Location loc2 = block.getLocation();	
-				loc.setX(loc2.getX() - 1);	
-				Block x2 = loc2.getWorld().getBlockAt(loc2);
-				
-				Location loc3 = block.getLocation();	
-				loc.setX(loc3.getX() - 1);	
-				Block z1 = loc3.getWorld().getBlockAt(loc3);
-				
-				Location loc4 = block.getLocation();	
-				loc.setX(loc4.getX() - 1);	
-				Block z2 = loc4.getWorld().getBlockAt(loc4);
-				
-				if (x1.getType() == Material.CHEST){
+				if (block.getType() == Material.IRON_BLOCK){
 					
-					Chest chest = (Chest) x1.getState();
+				    EntityPlayer entityPlayer = ((CraftPlayer)event.getPlayer()).getHandle();
 					
+					Location loc = block.getLocation();	
+					loc.setX(loc.getX() + 1);	
+					Block x1 = loc.getWorld().getBlockAt(loc);
+					
+					Location loc2 = block.getLocation();	
+					loc2.setX(loc2.getX() - 1);	
+					Block x2 = loc2.getWorld().getBlockAt(loc2);
+					
+					Location loc3 = block.getLocation();	
+					loc3.setZ(loc3.getZ() + 1);	
+					Block z1 = loc3.getWorld().getBlockAt(loc3);
+					
+					Location loc4 = block.getLocation();	
+					loc4.setZ(loc4.getZ() - 1);	
+					Block z2 = loc4.getWorld().getBlockAt(loc4);
+					
+					if (x1.getType() == Material.CHEST){
+						
 						if(x1.getState() instanceof ContainerBlock)
 						{
-
+	
 							openChest(x1,entityPlayer);
-							
+								
 						}
-					
-					chest.getInventory().addItem(new ItemStack(Material.WOOD,64));
-					
-				}else if (x2.getType() == Material.CHEST){
-					
-					Chest chest = (Chest) x2.getState();
-					
-					if(x2.getState() instanceof ContainerBlock)
-					{
-
-						openChest(x2,entityPlayer);
+						
+					}else if (x2.getType() == Material.CHEST){
+	
+							if(x2.getState() instanceof ContainerBlock)
+							{
+	
+								openChest(x2,entityPlayer);
+							
+							}
+						
+					}else if (z1.getType() == Material.CHEST){
+						
+							if(z1.getState() instanceof ContainerBlock)
+							{
+		
+								openChest(z1,entityPlayer);
+								
+							}
+						
+					}else if (z2.getType() == Material.CHEST){
+						
+							if(z2.getState() instanceof ContainerBlock)
+							{
+		
+								openChest(z2,entityPlayer);
+								
+							}
 						
 					}
-					
-					chest.getInventory().addItem(new ItemStack(Material.WOOD,64));
-					
-				}else if (z1.getType() == Material.CHEST){
-					
-					Chest chest = (Chest) z1.getState();
-					
-					if(z1.getState() instanceof ContainerBlock)
-					{
-
-						openChest(z1,entityPlayer);
-						
-					}
-					
-					chest.getInventory().addItem(new ItemStack(Material.WOOD,64));
-					
-				}else if (z2.getType() == Material.CHEST){
-					
-					Chest chest = (Chest) z2.getState();
-					
-					if(z2.getState() instanceof ContainerBlock)
-					{
-
-						openChest(z2,entityPlayer);
-						
-					}
-					
-					chest.getInventory().addItem(new ItemStack(Material.WOOD,64));
-					
 				}
-				
 			}
-			
 		}
 	}			
     
