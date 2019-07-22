e4ad28b7d3b8da0c475215c861cf4ba8cee9b8be
SimonIT <simonit.orig@gmail.com>
2019-07-22 14:31:01 +0200

    remove deprecated Durability usage

--- src/com/servegame/n1p/Fridges/FridgesPlayerListener.java
+++ src/com/servegame/n1p/Fridges/FridgesPlayerListener.java
@@ -11,28 +11,29 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
 
 public class FridgesPlayerListener implements Listener {
-
-    public Fridges plugin;
-
-    public FridgesPlayerListener(Fridges instance) {
-        plugin = instance;
-    }
 
     public final int woodDurability = 59;
     public final int diamondDurability = 1561;
     public final int stoneDurability = 131;
     public final int goldDurability = 32;
     public final int ironDurability = 250;
-
     public final Material[] woodMaterial = {Material.WOODEN_AXE, Material.WOODEN_PICKAXE, Material.WOODEN_SHOVEL, Material.WOODEN_HOE, Material.WOODEN_SWORD, Material.FISHING_ROD, Material.CARROT_ON_A_STICK, Material.BOW};
     public final Material[] diamondMaterial = {Material.DIAMOND_AXE, Material.DIAMOND_PICKAXE, Material.DIAMOND_SHOVEL, Material.DIAMOND_HOE, Material.DIAMOND_SWORD};
     public final Material[] stoneMaterial = {Material.STONE_AXE, Material.STONE_PICKAXE, Material.STONE_SHOVEL, Material.STONE_HOE, Material.STONE_SWORD};
     public final Material[] goldMaterial = {Material.GOLDEN_AXE, Material.GOLDEN_PICKAXE, Material.GOLDEN_SHOVEL, Material.GOLDEN_HOE, Material.GOLDEN_SWORD};
     public final Material[] ironMaterial = {Material.IRON_AXE, Material.IRON_PICKAXE, Material.IRON_SHOVEL, Material.IRON_HOE, Material.IRON_SWORD, Material.SHEARS, Material.FLINT_AND_STEEL};
+    public Fridges plugin;
+
+    public FridgesPlayerListener(Fridges instance) {
+        plugin = instance;
+    }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
@@ -70,57 +71,73 @@ public class FridgesPlayerListener implements Listener {
                         } else if (y1.getType() == Material.STONE_PRESSURE_PLATE
                                 && entityPlayer.hasPermission("fridges.anvil.repair")) {
 
-                            Material item = event.getItem().getType();
+                            ItemStack item = event.getItem();
 
-                            double price_per_durability = 0;
-                            String material = null;
+                            if (item != null) {
+                                Material itemMaterial = item.getType();
 
-                            if (Arrays.asList(woodMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.wood") / woodDurability;
-                                material = "wood";
-                            } else if (Arrays.asList(diamondMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.diamond") / diamondDurability;
-                                material = "diamond";
-                            } else if (Arrays.asList(stoneMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.stone") / stoneDurability;
-                                material = "stone";
-                            } else if (Arrays.asList(goldMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.gold") / goldDurability;
-                                material = "gold";
-                            } else if (Arrays.asList(ironMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.iron") / ironDurability;
-                                material = "iron";
-                            }
+                                double price_per_durability = 0;
+                                String material = null;
 
-                            if (price_per_durability != 0) {
-                                short durability = event.getItem().getDurability();
+                                if (Arrays.asList(woodMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.wood") / woodDurability;
+                                    material = "wood";
+                                } else if (Arrays.asList(diamondMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.diamond") / diamondDurability;
+                                    material = "diamond";
+                                } else if (Arrays.asList(stoneMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.stone") / stoneDurability;
+                                    material = "stone";
+                                } else if (Arrays.asList(goldMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.gold") / goldDurability;
+                                    material = "gold";
+                                } else if (Arrays.asList(ironMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.iron") / ironDurability;
+                                    material = "iron";
+                                }
 
-                                if (durability == 0) {
-                                    entityPlayer.sendMessage("Tool at full durability.");
-                                } else {
-                                    double price = price_per_durability * durability;
-                                    if (Fridges.economy.getBalance(entityPlayer) < price) {
-                                        double money = Fridges.economy.getBalance(entityPlayer);
+                                if (price_per_durability != 0) {
+                                    int durability = 0;
+                                    ItemMeta meta = item.getItemMeta();
+                                    if (meta != null) {
+                                        durability = ((Damageable) meta).getDamage();
+                                    }
 
-                                        if (money != 0) {
-                                            durability -= (short) (money / price_per_durability);
-                                            price = price_per_durability * (money / price_per_durability);
-                                            event.getItem().setDurability(durability);
+                                    if (durability == 0) {
+                                        entityPlayer.sendMessage("Tool at full durability.");
+                                    } else {
+                                        double price = price_per_durability * durability;
+                                        if (Fridges.economy.getBalance(entityPlayer) < price) {
+                                            double money = Fridges.economy.getBalance(entityPlayer);
+
+                                            if (money != 0) {
+                                                durability -= (short) (money / price_per_durability);
+                                                price = price_per_durability * (money / price_per_durability);
+                                                meta = item.getItemMeta();
+                                                if (meta != null) {
+                                                    ((Damageable) meta).setDamage(durability);
+                                                    item.setItemMeta(meta);
+                                                }
+                                                Fridges.economy.withdrawPlayer(entityPlayer, price);
+                                                entityPlayer.sendMessage(
+                                                        "Repaired your " + material + " tool for " + price + " Crates!");
+                                            } else {
+                                                entityPlayer.sendMessage(
+                                                        "You have no money to repair your " + material + " tool");
+                                            }
+                                        } else {
+                                            meta = item.getItemMeta();
+                                            if (meta != null) {
+                                                ((Damageable) meta).setDamage(0);
+                                                item.setItemMeta(meta);
+                                            }
                                             Fridges.economy.withdrawPlayer(entityPlayer, price);
                                             entityPlayer.sendMessage(
                                                     "Repaired your " + material + " tool for " + price + " Crates!");
-                                        } else {
-                                            entityPlayer.sendMessage(
-                                                    "You have no money to repair your " + material + " tool");
                                         }
-                                    } else {
-                                        event.getItem().setDurability((short) 0);
-                                        Fridges.economy.withdrawPlayer(entityPlayer, price);
-                                        entityPlayer.sendMessage(
-                                                "Repaired your " + material + " tool for " + price + " Crates!");
                                     }
+                                    event.setCancelled(true);
                                 }
-                                event.setCancelled(true);
                             }
                         }
                     }
@@ -141,40 +158,48 @@ public class FridgesPlayerListener implements Listener {
                         loc.setY(loc.getY() - 1);
                         Block y1 = loc.getWorld().getBlockAt(loc);
 
-                        Material item = event.getItem().getType();
+                        ItemStack item = event.getItem();
+                        if (item != null) {
+                            Material itemMaterial = item.getType();
 
-                        if (y1.getType() == Material.STONE_PRESSURE_PLATE && entityPlayer.hasPermission("fridges.anvil.info")) {
-                            double price_per_durability = 0;
-                            String material = null;
+                            if (y1.getType() == Material.STONE_PRESSURE_PLATE && entityPlayer.hasPermission("fridges.anvil.info")) {
+                                double price_per_durability = 0;
+                                String material = null;
 
-                            if (Arrays.asList(woodMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.wood") / woodDurability;
-                                material = "wood";
-                            } else if (Arrays.asList(diamondMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.diamond") / diamondDurability;
-                                material = "diamond";
-                            } else if (Arrays.asList(stoneMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.stone") / stoneDurability;
-                                material = "stone";
-                            } else if (Arrays.asList(goldMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.gold") / goldDurability;
-                                material = "gold";
-                            } else if (Arrays.asList(ironMaterial).contains(item)) {
-                                price_per_durability = this.plugin.getConfig().getDouble("anvils.price.iron") / ironDurability;
-                                material = "iron";
-                            }
-
-                            if (price_per_durability != 0) {
-                                short durability = event.getItem().getDurability();
-
-                                if (durability == 0) {
-                                    entityPlayer.sendMessage("Tool at full durability.");
-                                } else {
-                                    double price = price_per_durability * durability;
-                                    entityPlayer.sendMessage(
-                                            "It will cost " + price + " Crates to repair your " + material + " tool!");
+                                if (Arrays.asList(woodMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.wood") / woodDurability;
+                                    material = "wood";
+                                } else if (Arrays.asList(diamondMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.diamond") / diamondDurability;
+                                    material = "diamond";
+                                } else if (Arrays.asList(stoneMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.stone") / stoneDurability;
+                                    material = "stone";
+                                } else if (Arrays.asList(goldMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.gold") / goldDurability;
+                                    material = "gold";
+                                } else if (Arrays.asList(ironMaterial).contains(itemMaterial)) {
+                                    price_per_durability = this.plugin.getConfig().getDouble("anvils.price.iron") / ironDurability;
+                                    material = "iron";
                                 }
-                                event.setCancelled(true);
+
+                                if (price_per_durability != 0) {
+                                    int durability = 0;
+
+                                    ItemMeta meta = item.getItemMeta();
+                                    if (meta != null) {
+                                        durability = ((Damageable) meta).getDamage();
+                                    }
+
+                                    if (durability == 0) {
+                                        entityPlayer.sendMessage("Tool at full durability.");
+                                    } else {
+                                        double price = price_per_durability * durability;
+                                        entityPlayer.sendMessage(
+                                                "It will cost " + price + " Crates to repair your " + material + " tool!");
+                                    }
+                                    event.setCancelled(true);
+                                }
                             }
                         }
                     }
